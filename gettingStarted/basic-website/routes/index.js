d473a3f00120dac554135f204a38901281d1c799
Raushan Raja <vraushanraja@gmail.com>
2019-01-05 20:37:52 +0530

    added routes

--- gettingStarted/basic-website/routes/index.js
+++ gettingStarted/basic-website/routes/index.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res) =>{
-    res.send("Hello from routes/index.js ");
+    res.render('index',{title:'Hexweb'});
 });
 
+router.post('/b',(req,res) =>{
+    res.send("Hello");
+    // res.send(req.body.title);
+});
+
+router.post('/bo',(req,res) =>{
+    res.send("Hello");
+    var i =req.body;
+    console.log(i);
+});
 module.exports= router;
