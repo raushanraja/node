8375596637f0d1503e293ae26dcfe71de9e94c66
raushanraja <vraushanraja@gmail.com>
2019-11-26 16:37:44 +0530

    singleton_example

---
+++ hello.js
@@ -0,0 +1,12 @@
+const name = "Raushan";
+function helloWorld() {
+    let lastname;
+    if(!lastname){
+        lastname="Raja";
+        console.log("Inside IF-ELSE");
+    }
+
+    return name+" "+lastname;
+}
+
+module.exports=helloWorld();
---
+++ index.js
@@ -0,0 +1,5 @@
+const name1 = require("./hello");
+const name2 = require("./hello");
+const name3 = require("./hello");
+
+console.log(name1,name1,name3);
